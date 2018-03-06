5f53a4f422f275a85f19ebdc7834840b2ead59e5
Selsen <Selsen@users.noreply.github.com>
2018-03-06 16:42:15 +0100

    Add files via upload

--- board.js
+++ board.js
@@ -1,6 +1,7 @@
-class Board {
+export class Board {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
     this._numberOfBombs = numberOfBombs;
+
     this._numberOfTiles = numberOfRows * numberOfColumns;
     this._playerBoard = Board.generatePlayerBoard(numberOfRows, numberOfColumns);
     this._bombBoard = Board.generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs);
@@ -13,7 +14,8 @@ class Board {
   flipTile(rowIndex, columnIndex) {
     if (this._playerBoard[rowIndex][columnIndex] !== ' ') {
       return "This tile has already been flipped!";
-    } else if (this._bombBoard[rowIndex][columnIndex] === 'B') {
+    }
+    else if (this._bombBoard[rowIndex][columnIndex] === 'B') {
       this._playerBoard[rowIndex][columnIndex] = 'B';
     } else {
       this._playerBoard[rowIndex][columnIndex] = this.getNumberOfNeighborBombs(rowIndex, columnIndex);
